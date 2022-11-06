efd429966f18f54d94345ed7a1f230b476f73356
ZIDNI MUJIB <zidni.mujib5@gmail.com>
2022-11-07 06:29:06 +0700

    add query param & multipart/form-data

--- controller/ExampleControllerImpl.go
+++ controller/ExampleControllerImpl.go
@@ -5,9 +5,14 @@ import (
 	"golang-basic-http/model/request"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
 type ExampleControllerImpl struct{}
+
+const (
+	MESSAGE = "Oops the method is doesn exist"
+)
 
 func (e ExampleControllerImpl) Index(w http.ResponseWriter, r *http.Request) {
 	message := "Oops the method is doesn exist"
@@ -37,7 +42,6 @@ func (e ExampleControllerImpl) Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func (e ExampleControllerImpl) Store(w http.ResponseWriter, r *http.Request) {
-	message := "Oops the method is doesn exist"
 	switch r.Method {
 	case "POST":
 		var exampleModel request.ExampleRequest
@@ -59,6 +63,62 @@ func (e ExampleControllerImpl) Store(w http.ResponseWriter, r *http.Request) {
 		}
 	default:
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(message))
+		w.Write([]byte(MESSAGE))
+	}
+}
+
+func (e ExampleControllerImpl) Find(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		id := r.URL.Query().Get("id")
+
+		response := map[string]interface{}{
+			"success": true,
+			"message": "Ok",
+			"data": map[string]string{
+				"id": id,
+			},
+		}
+
+		w.WriteHeader(200)
+
+		err := json.NewEncoder(w).Encode(response)
+		if err != nil {
+			panic(err)
+		}
+
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(MESSAGE))
+	}
+
+}
+
+func (e ExampleControllerImpl) Multipart(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		var data request.ExampleRequest
+		data = request.ExampleRequest{}
+		r.ParseMultipartForm(0)
+
+		data.Name = r.FormValue("name")
+		age, err := strconv.Atoi(r.FormValue("age"))
+		if err != nil {
+			panic(err.Error())
+		}
+
+		data.Age = age
+
+		response := map[string]interface{}{
+			"success": true,
+			"message": "Ok",
+			"data":    data,
+		}
+
+		w.WriteHeader(200)
+		err = json.NewEncoder(w).Encode(response)
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(MESSAGE))
 	}
 }
